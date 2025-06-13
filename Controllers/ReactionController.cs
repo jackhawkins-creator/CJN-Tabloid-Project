6e2e5f902c30084c8250983574b34f17af160d99
Jack Hawkins <jack.m.hawkins@vanderbilt.edu>
2025-06-13 15:30:50 -0500

    homepage and my posts now link to post details

--- Controllers/ReactionController.cs
+++ Controllers/ReactionController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 using Tabloid.Data;
+using Tabloid.Models;
 
 
 namespace Tabloid.Controllers;
@@ -25,5 +26,42 @@ public class ReactionController : ControllerBase
     {
         var Reactions = _dbContext.Reactions;
         return Ok(Reactions);
+    }
+
+    //POST Reaction
+    //Raw Body Test Data:
+    /*
+    {
+  "postId": 1,
+  "userProfileId": 2,
+  "emoji": "🔥"
+    }
+    */
+    [HttpPost]
+    //[Authorize]
+    public IActionResult CreateReaction(Reaction reaction)
+    {
+        reaction.ReactedOn = DateTime.Now;
+        _dbContext.Reactions.Add(reaction);
+        _dbContext.SaveChanges();
+        return Created($"/api/reaction/{reaction.Id}", reaction);
+    }
+
+    //DELETE Reaction
+    [HttpDelete("{id}")]
+    //[Authorize]
+    public IActionResult DeleteReaction(int id)
+    {
+        Reaction reaction = _dbContext.Reactions.SingleOrDefault(r => r.Id == id);
+
+        if (reaction == null)
+        {
+            return NotFound();
+        }
+
+        _dbContext.Reactions.Remove(reaction);
+        _dbContext.SaveChanges();
+
+        return NoContent();
     }
 }
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,61 +1,76 @@
-import { useEffect, useState } from "react"
-import { getAllPosts } from "../components/managers/postManager"
-import { getAllUserProfiles } from "../Components/managers/userProfileManager"
+import { useEffect, useState } from "react";
+import { getAllPosts } from "../Components/managers/postManager";
+import { getAllUserProfiles } from "../Components/managers/userProfileManager";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
+  const [allPosts, setAllPosts] = useState([]);
+  const [allAuthors, setAllAuthors] = useState([]);
 
-    const [allPosts, setAllPosts] = useState([])
-    const [allAuthors, setAllAuthors] = useState([])
+  useEffect(() => {
+    getAllPosts().then(setAllPosts), getAllUserProfiles().then(setAllAuthors);
+  }, []);
 
-    useEffect(() => {
-        getAllPosts().then(setAllPosts),
-        getAllUserProfiles().then(setAllAuthors)
-    }, [])
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const year = date.getFullYear();
 
-    const formatDate = (dateString) => {
-        const date = new Date(dateString)
-        const month = String(date.getMonth() + 1).padStart(2,"0")
-        const day = String(date.getDate()).padStart(2, "0")
-        const year = date.getFullYear()
+    return `${month}/${day}/${year}`;
+  };
 
-        return `${month}/${day}/${year}`
-    }
+  return (
+    <>
+      <div className="posts-container">
+        <h1>Latest Posts</h1>
+        {allPosts.slice(0, 1).map((post) => (
+          <div key={post.id} className="most-recent-post">
+            <Link to={`/posts/${post.id}`}>
+              <img src={post.headerImageUrl} alt={post.title} />
+              <h3 className="post-title">{post.title}</h3>
+            </Link>
+            <p className="post-body">{post.body}</p>
+            <div className="most-recent-footer">
+              <p className="post-published">
+                Published {formatDate(post.publishingDate)}
+              </p>
+            </div>
+          </div>
+        ))}
 
-    return (
-        <>
-        <div className="posts-container">
-            <h1>Latest Posts</h1>
-            {allPosts.slice(0,1).map((post) => 
-            <div key={post.id} className="most-recent-post">
-                <img src={post.headerImageUrl} />
+        <div className="recent-posts">
+          {allPosts.slice(1, 3).map((post) => (
+            <div className="recent-post" key={post.id}>
+              <Link to={`/posts/${post.id}`}>
+                <img
+                  src={post.headerImageUrl}
+                  className="header-image"
+                  alt={post.title}
+                />
                 <h3 className="post-title">{post.title}</h3>
-                <p className="post-body">{post.body}</p>
-                <div className="most-recent-footer">
-                    <p className="post-published">Published {formatDate(post.publishingDate)}</p>
-                </div>
+              </Link>
+              <p className="post-body">{post.body}</p>
+              <div className="post-footer">
+                <p className="post-published">
+                  Published {formatDate(post.publishingDate)}
+                </p>
+              </div>
             </div>
-            )}
-            <div className="recent-posts">
-                {allPosts.slice(1,3).map((post) => 
-                <div className="recent-post" key={post.id}>
-                    <img src={post.headerImageUrl} className="header-image" />
-                    <h3 className="post-title">{post.title}</h3>
-                    <p className="post-body">{post.body}</p>
-                    <div className="post-footer">
-                        <p className="post-published">Published {formatDate(post.publishingDate)}</p>
-                    </div>
-                </div>)}
-            </div>
+          ))}
         </div>
-        <div className="author-container">
-            <h2>New Authors</h2>
-            {allAuthors.slice(0,8).map((author) => 
-            <div className="author" key={author.id}>
-                <div className="author-name">{author.firstName} {author.lastName}</div>
-                <p className="author-join-date">{author.timeSinceJoin}</p>
+      </div>
+      <div className="author-container">
+        <h2>New Authors</h2>
+        {allAuthors.slice(0, 8).map((author) => (
+          <div className="author" key={author.id}>
+            <div className="author-name">
+              {author.firstName} {author.lastName}
             </div>
-            )}
-        </div>
-        </>
-    )
-}
+            <p className="author-join-date">{author.timeSinceJoin}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
